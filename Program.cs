0c74b6926939dc52ccb0ceec3e2a0efb661d17f5
Matt1300 <mateogranizo1304@gmail.com>
2024-10-12 18:23:19 -0500

    Probando Triggers en GitHub Actions

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿string userName = Environment.GetEnvironmentVariable("GITHUB_USER") ?? "Usuario desconocido";
-Console.WriteLine($"¡Hola {userName} desde GitHub!");
+﻿string userName = Environment.GetEnvironmentVariable("GITHUB_USER") ?? Environment.GetEnvironmentVariable("USERNAME");
+string language = Environment.GetEnvironmentVariable("LANGUAGE");
+
+Console.WriteLine($"¡Hola {userName}, este es un codigo en {language}");
