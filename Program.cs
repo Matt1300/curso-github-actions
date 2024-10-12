23b9eba7bb53458dda45e6c32af03ed681d68696
Matt1300 <mateogranizo1304@gmail.com>
2024-10-12 17:31:52 -0500

    Testing variables en workflow

--- Program.cs
+++ Program.cs
@@ -1,1 +1,2 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string userName = Environment.GetEnvironmentVariable("GITHUB_USER") ?? "Usuario desconocido";
+Console.WriteLine($"¡Hola {userName} desde GitHub!");
