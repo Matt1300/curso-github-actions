588a42a8d17005d21dcf241f8a2e553cb275b2e9
Matt1300 <mateogranizo1304@gmail.com>
2024-10-12 18:35:22 -0500

    Prueba triggers

--- Program.cs
+++ Program.cs
@@ -1,12 +1,4 @@
-﻿string userNameGitHub = Environment.GetEnvironmentVariable("GITHUB_USER");
-string userName = Environment.GetEnvironmentVariable("USERNAME");
+﻿string userName = Environment.GetEnvironmentVariable("USERNAME");
 string language = Environment.GetEnvironmentVariable("LANGUAGE");
 
-if (userNameGitHub is not null)
-{
-    Console.WriteLine($"¡Hola {userNameGitHub} desde GitHub");
-}
-else
-{
-    Console.WriteLine($"¡Hola {userName}, este es un codigo en {language}");
-}
+Console.WriteLine($"¡Hola {userName}, este es un codigo en {language}");
